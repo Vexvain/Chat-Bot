2cc7436d97b95c34f3ee4cef54d4ce85575e00fd
Vexvain <60254325+Vexvain@users.noreply.github.com>
2020-09-04 20:57:17 -0400

    Update main.py

--- main.py
+++ main.py
@@ -82,3 +82,36 @@ except:
     model.save("model.tflearn")
 
 def bag_of_words(s, words):
+    bag = [0 for _ in range(len(words))]
+
+    s_words = nltk.word_tokenize(s)
+    s_words = [stemmer.stem(word.lower()) for word in s_words]
+
+    for se in s_words:
+        for i, w in enumerate(words):
+            if w == se:
+                bag[i] = 1
+            
+    return numpy.array(bag)
+
+
+def chat():
+    print("Start chatting with the bot (type quit to stop)!")
+    while True:
+        inp = input("You: ")
+        if inp.lower() == "quit":
+            break
+
+        results = model.predict([bag_of_words(inp, words)])
+        results_index = numpy.argmax(results)
+        tag = labels[results_index]
+        
+        if results[results_index] > 0.6:
+            for tg in data["intents"]:
+                if tg['tag'] == tag:
+                    responses = tg['responses']
+
+        print(random.choice(responses))
+    else:
+        print("I don't quite understand what you're saying, sorry. :(")
+chat()
